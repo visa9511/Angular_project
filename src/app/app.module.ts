141ceb60984064d5f40b15653ee43b9cf011200f
Ervisa <ervisamurataj1@gmail.com>
2024-02-15 15:18:09 +0100

    Implemented counter components with Bootstrap styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ButtoncomponentComponent } from './buttoncomponent/buttoncomponent.component';
+import { MycomponentComponent } from './mycomponent/mycomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtoncomponentComponent,
+    MycomponentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/buttoncomponent/buttoncomponent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buttoncomponent',
+  templateUrl: './buttoncomponent.component.html',
+  styleUrls: ['./buttoncomponent.component.css']
+})
+export class ButtoncomponentComponent {
+
+}
